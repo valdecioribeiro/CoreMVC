e7fa39ae1f6297fb60cfa862a1b6e749bde2cfdd
Valdecio Borges Ribeiro <valdecio@gmail.com>
2020-06-16 09:03:26 -0300

    CRUD scafolding

---
+++ CoreMVC/Data/CoreMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CoreMVC.Models;
+
+namespace CoreMVC.Data
+{
+    public class CoreMVCContext : DbContext
+    {
+        public CoreMVCContext (DbContextOptions<CoreMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreMVC.Models.Departamento> Departamento { get; set; }
+    }
+}
--- CoreMVC/Startup.cs
+++ CoreMVC/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CoreMVC.Data;
 
 namespace CoreMVC
 {
@@ -24,6 +26,9 @@ namespace CoreMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<CoreMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CoreMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
