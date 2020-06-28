5c2158ad215082201998ed52f35f7a3e796014b5
Valdecio Borges Ribeiro <valdecio@gmail.com>
2020-06-28 14:56:40 -0300

    Preparando a navegação de telas de pesquisa de vendas

---
+++ CoreMVC/Controllers/RegistroVendasController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVC.Controllers
+{
+    public class RegistroVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcuraSimples()
+        {
+            return View();
+        }
+
+        public IActionResult ProcuraAgrupada()
+        {
+            return View();
+        }
+    }
+}
